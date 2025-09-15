c683ba5da810d2c4a856fa27c9bf01930536f0ff
Ethem-Deli <e.deli5595@gmail.com>
2025-09-15 14:40:08 +0300

    W02 Project: Contacts Part 2

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,9 @@
+
 const mongodb = require("../data/database");
 const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res) => {
+  //#swagger.tags=["Users"]
   try {
     const db = mongodb.getDatabase();
     const result = db.collection("users").find();
@@ -15,6 +17,7 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
+  //#swagger.tags=["Users"]
   try {
     const userId = new ObjectId(req.params.id);
     const db = mongodb.getDatabase();
@@ -27,8 +30,55 @@ const getSingle = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+const createUser = async (req, res) => {
+  //#swagger.tags=["Users"]
+  const user = {
+    firstName:req.body.firstName,
+    lastName:req.body.lastName,
+    email:req.body.email,
+    favoriteColor:req.body.favoriteColor,
+    birthday:req.body.birthday
+  };
+  const response = await mongodb.getDatabase().db().collection("users").insertOne(user);
+  if (response.acknowledged) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || "Some error occurred while creating the user.");
+  }
+};
+
+const updateUser = async (req, res) => {
+  //#swagger.tags=["Users"]
+  const userId = new ObjectId(req.params.id);
+  const user = {
+    username:req.body.username,
+    email:req.body.email,
+    name:req.body.name,
+    ipaddress:req.body.ipaddress,
+  };
+  const response = await mongodb.getDatabase().db().collection("users").replaceOne({_id: userId },user);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || "Some error occurred while updating the user.");
+  };
+};    
+
+const deleteUser = async (req, res) => {
+  //#swagger.tags=["Users"]
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb.getDatabase().db().collection("users").deleteOne({_id: userId });
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || "Some error occurred while deleting the user.");
+  };
+};
 
 module.exports = {
   getAll,
   getSingle,
+  createUser,
+  updateUser,
+  deleteUser
 };
--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const mongodb = require("./data/database");
-const app = express();
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
+const app = express();
 const port = process.env.PORT || 3000;
-// Use routes
+
+app.use(bodyParser.json());
+
+// CORS headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+// Swagger mouted here
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Routes
 app.use("/", require("./routes"));
 
 app.get("/", (req, res) => {
