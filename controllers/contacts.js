f07303543b6ca798f63487f9093c7d4327227850
Ethem-Deli <e.deli5595@gmail.com>
2025-09-21 10:11:26 +0300

    ADDING ERROR HANDLING(video lesson)

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,96 +1,102 @@
-const mongodb = require("../data/database");
-const ObjectId = require("mongodb").ObjectId;
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
 
 // Get all contacts
-const getAll = async (req, res) => {
+const getAll = (req, res) => {
   //#swagger.tags=["Contacts"]
-  try {
-    const result = await mongodb.getDatabase().collection("contacts").find();
-    result.toArray().then((contacts) => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(200).json(contacts);
+  mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .find()
+    .toArray((err, lists) => {
+      if (err) {
+        res.status(400).json({ message: err });
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists);
     });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
 };
 
-// Get single contact
-const getSingle = async (req, res) => {
+// Get a single contact by its ID
+const getSingle = (req, res) => {
   //#swagger.tags=["Contacts"]
-  try {
-    const contactId = new ObjectId(req.params.id);
-    const result = await mongodb
-      .getDatabase()
-      .collection("contacts")
-      .find({ _id: contactId });
-    result.toArray().then((contacts) => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(200).json(contacts[0]);
-    });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid contact id to find a contact.');
   }
+  const userId = new ObjectId(req.params.id);
+  mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .find({ _id: userId })
+    .toArray((err, result) => {
+      if (err) {
+        res.status(400).json({ message: err });
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(result[0]);
+    });
 };
-
+// Create a new contact
 const createContact = async (req, res) => {
   //#swagger.tags=["Contacts"]
-  try {
-    const contact = {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      favoriteColor: req.body.favoriteColor,
-      birthday: req.body.birthday,
-    };
-
-    const response = await mongodb.getDatabase().collection("contacts").insertOne(contact);
-    if (response.acknowledged) {
-      res.status(201).json({ id: response.insertedId });
-    } else {
-      res.status(500).json({ message: "Error creating contact" });
-    }
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
   }
 };
-// Update contact
+
+// Update a contact by its ID
 const updateContact = async (req, res) => {
   //#swagger.tags=["Contacts"]
-  try {
-    const contactId = new ObjectId(req.params.id);
-    const contact = {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      favoriteColor: req.body.favoriteColor,
-      birthday: req.body.birthday,
-    };
-
-    const response = await mongodb.getDatabase().collection("contacts").replaceOne({ _id: contactId }, contact);
-    if (response.modifiedCount > 0) {
-      res.status(204).send();
-    } else {
-      res.status(404).json({ message: "Contact not found" });
-    }
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid contact id to update a contact.');
+  }
+  const userId = new ObjectId(req.params.id);
+  // be aware of updateOne if you only want to update specific fields
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .replaceOne({ _id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
   }
 };
-// Delete contact
+
+// Delete a contact by its ID
 const deleteContact = async (req, res) => {
   //#swagger.tags=["Contacts"]
-  try {
-    const contactId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().collection("contacts").deleteOne({ _id: contactId });
-
-    if (response.deletedCount > 0) {
-      res.status(204).send();
-    } else {
-      res.status(404).json({ message: "Contact not found" });
-    }
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid contact id to delete a contact.');
+  }
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
   }
 };
 
--- server.js
+++ server.js
@@ -1,45 +1,26 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongodb = require("./data/database");
-
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger.json");
-
-const app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
 const port = process.env.PORT || 3000;
+const app = express();
 
-app.use(bodyParser.json());
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
 
-// CORS headers
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
-  );
-  next();
-});
-
-// Swagger mouted here
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// Routes
-app.use("/", require("./routes"));
-
-app.get("/", (req, res) => {
-  res.redirect("/users");
+process.on('uncaughtException', (err, origin) => {
+  console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
 });
 
 mongodb.initDb((err) => {
   if (err) {
     console.log(err);
   } else {
-    app.listen(port, () => {
-      console.log(`Database is Listening and node Running on port ${port}`);
-    });
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
   }
 });
